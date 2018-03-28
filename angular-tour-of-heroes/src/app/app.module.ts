f20f3b1ac0f3654e259f96244ea45facdaab7c90
yangmqCM <15827408750@163>
2018-03-28 17:09:48 +0800

    detail

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,18 +4,22 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+  	HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES:Hero[]=[
+ {id:2,name:'html5/css3'},
+ {id:3,name:'微信小程序vue'},
+ {id:4,name:'js深度学习'},
+ {id:5,name:'webscoket'},
+ {id:6,name:'单元测试，end to end'},
+ {id:7,name:'各种调试工具'},
+ {id:8,name:'tcp-TP/HTTP,抓包工具'},
+ {id:9,name:'echarts,highcharts'}, 
+ {id:10,name:'npm/node'},
+ {id:11,name:'前端自动化'}
+]
